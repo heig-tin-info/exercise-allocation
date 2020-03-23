3d022ba94914aee4217dfa82b6f87231dc01d6d1
Yves Chevallier <nowox@x0x.ch>
2020-03-23 17:57:31 +0100

    Bugfixes

--- sort.c
+++ sort.c
@@ -16,17 +16,21 @@ int compare(const void* a, const void* b)
 int main(int argc, char *argv[]) {
     int array[10]; 
 
+    // Parses arguments and consistency checks...
     if (argc < 2) return 1;
     int elements = atoi(argv[1]); 
 
     if (elements >= sizeof(array)/sizeof(array[0])) return 2;
 
+    // Read values from stdin
     for (int i = 0; i < elements; i++) {
         scanf("%d", &array[i]);
     }
 
+    // Sort values
     qsort(array, sizeof(array)/sizeof(int), sizeof(int), compare);
 
+    // Display
     for (int i = 0; i < elements; i++) {
         printf("%d\n", array[i]);
     }    
