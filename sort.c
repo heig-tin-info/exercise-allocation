7ac9a464598f68f35e6b18e6065e469ca955710a
Yves Chevallier <nowox@x0x.ch>
2020-03-23 20:05:14 +0100

    Abstract Data Type (ADT)

--- sort.c
+++ sort.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Buffer {
+    size_t buffer_size; // Double each time we reallocate
+    size_t elements; // Number of elements in the buffer
+    int *data;
+} Buffer;
+
+int *init(Buffer *buf) {
+    buf->buffer_size = 1;
+    buf->elements = 0;
+    buf->data = malloc(buf->buffer_size * sizeof(int));
+    return buf->data;
+}
+
+int append(Buffer *buf, int data) {
+    if (buf->data == NULL) return 1;
+    buf->data[buf->elements++] = data;
+    if (buf->elements > buf->buffer_size) {
+        buf->data = realloc(buf->data, buf->buffer_size <<= 1);
+        if (buf->data == NULL) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int pop(Buffer *buf, int *data) {
+    if (buf->elements == 0) {
+        return 0;
+    }
+    if (buf->elements < buf->buffer_size / 2) {
+        buf->data = realloc(buf->data, buf->buffer_size >>= 1);
+        if (buf->data == NULL) {
+            return 0;
+        }        
+    }
+    *data = buf->data[--buf->elements];
+    return 1; 
+}
+
 int compare(const void* a, const void* b)
 {
      int int_a = *((int*)a), int_b = *((int*)b);
@@ -8,33 +47,21 @@ int compare(const void* a, const void* b)
      if (int_a == int_b)
         return 0;
      else if (int_a < int_b)
-        return -1;
-     else
         return 1;
+     else
+        return -1;
+}
+
+void sort(Buffer *buf) {
+    qsort(buf->data, buf->elements, sizeof(int), compare);
 }
 
 int main(void) {
-    int buffer_size = 1;
-    int *array = malloc(sizeof(int) * buffer_size);
-    if (array == NULL) return 2;
+    Buffer buf;
+    if (!init(&buf)) return 2;
 
-    // Read values from stdin
-    int size = 0;
-    while(scanf("%d", &array[size]) > 0) {
-        if (size > buffer_size) {
-            array = realloc(array, buffer_size = size * 2);
-            if (array == NULL) return 2;
-        }
-        size++;
-    }
-
-    // Sort values
-    qsort(array, size, sizeof(int), compare);
-
-    // Display
-    for (int i = 0; i < size; i++) {
-        printf("%d\n", array[i]);
-    }    
-
-    free(array);
+    int num;
+    while(scanf("%d", &num) > 0 && append(&buf, num)) {}        
+    sort(&buf);
+    while(pop(&buf, &num)) printf("%d\n", num);
 }
